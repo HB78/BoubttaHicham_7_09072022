91da76b67100832f54dd7af3fa897e00a6c8d23e
HB78 <78313137+HB78@users.noreply.github.com>
2022-07-19 17:59:55 +0200

    mise en place des routes pour gerer le profil

--- back-end/controller/usersController.js
+++ back-end/controller/usersController.js
@@ -20,7 +20,7 @@ function validateEmail(email) {
 exports.signup = async (req, res, next) => {
     try {
         //si le user ne rentre pas de mdp, de nom ou de mail
-        if (!req.body.email || !req.body.password || !req.body.name ) {
+        if (!req.body.email || !req.body.password || !req.body.name) {
             res.status(400).send("bad request need email and password and name");
             return;
         }
@@ -48,7 +48,7 @@ exports.signup = async (req, res, next) => {
         //rows nous renvoi les données de la BDD cad un tabeau de ce qui a été enregistré
         console.log('--> rows', rows);
 
-        //si il n'y a un email dans le tableau donc email existant
+        //si il y a un email dans le tableau donc email existant
         //avec rows on interroge la BDD
         if (rows.length > 0) {
             res.status(401).json("L'email existe déjà");
@@ -64,65 +64,144 @@ exports.signup = async (req, res, next) => {
 
 //Login d'un utilisateur
 exports.login = async (req, res, next) => {
-  try {
-      //si l'utilisateur n'a pas entré de mdp ou de mail on gère l'erreur
-      if(!req.body.password || !req.body.email) {
-          res.status(400).json("veuillez entrez un mot de passe et un email")
-      }
+    try {
+        //si l'utilisateur n'a pas entré de mdp ou de mail on gère l'erreur
+        if (!req.body.password || !req.body.email) {
+            res.status(400).json("veuillez entrez un mot de passe et un email")
+        }
 
-      //la commande est mise dans une constante
-      //dans la constante on récupère tt les infos de la table users ayant le mail rentré par le user
-      const emailSQL = `SELECT * FROM users WHERE email='${req.body.email}'`
+        //la commande est mise dans une constante
+        //dans la constante on récupère tt les infos de la table users ayant le mail rentré par le user
+        const emailSQL = `SELECT * FROM users WHERE email='${req.body.email}'`
 
-      //on execute la commande avec db.query
-      //on va recuperer dans le rows le resultat de emailSQL
-      let [rows, fields] = await db.query(emailSQL)
+        //on execute la commande avec db.query
+        //on va recuperer dans le rows le resultat de emailSQL
+        let [rows, fields] = await db.query(emailSQL)
 
-      //dans le rows on a le resultat de la commande emailsql apres execution
-      //autrement dit le mail, le mdp et le nom du user avec le mail rentré issus de la BDD
-      console.log('rows:', rows)
-      console.log('mot de pass hasher', rows[0].password)
-      console.log('req.body.password', req.body.password)
-      // Comparer le mot de pass hash et mdp rentré par le user req.body.password avec bcrypt (comparsync)
-      // si c'est pas bon 401 sinon continuer le code générer JWT
+        //dans le rows on a le resultat de la commande emailsql apres execution
+        //autrement dit le mail, le mdp et le nom du user avec le mail rentré issus de la BDD
+        console.log('rows:', rows)
+        console.log('mot de pass hasher', rows[0].password)
+        console.log('req.body.password', req.body.password)
+        // Comparer le mot de pass hash et mdp rentré par le user req.body.password avec bcrypt (comparsync)
+        // si c'est pas bon 401 sinon continuer le code générer JWT
 
-      let passwordCompare = bcrypt.compareSync(req.body.password, rows[0].password)
-      if(passwordCompare == false) {
-          console.log("C'est pas le bon MDP")
-          return res.status(401).json({error: "mot de passe incorrect"})
-      }else {
-          console.log("C'est le bon MDP")
-          res.status(200).json("le mot de passe est correct");
-          const token = jwt.sign({ userEmail : rows[0].email }, process.env.KEY, {expiresIn: "77d"});
-          const objResponse = {
-              userEmail : rows[0].email,
-              token : token,
-          }
-          console.log(objResponse)
-      }
-  } catch (error) {
-      res.status(500).json(error);
-  }
-}
+        let passwordCompare = bcrypt.compareSync(req.body.password, rows[0].password)
+        if (passwordCompare == false) {
+            console.log("C'est pas le bon MDP")
+            return res.status(401).json({ error: "mot de passe incorrect" })
+        } else {
+            console.log("C'est le bon MDP")
+            res.status(200).json("le mot de passe est correct");
+            const token = jwt.sign({ userEmail: rows[0].email }, process.env.KEY, { expiresIn: "77d" });
+            const objResponse = {
+                userEmail: rows[0].email,
+                token: token,
+            }
+            console.log(objResponse)
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
-//Récupération de tous les utilisateurs id nom et photo
-exports.getusers = async (req, res) => {
-  try {
-     let query = 'SELECT * FROM users';
-     let [rows,fields] = await db.query(query);
-     let id = rows[0].id;
-     res.send(200).json(rows);
-   } catch (e) {
-     res.status(500).json(e);
-   } 
- };
+//Récupération de tous les utilisateurs id nom le poste et la photo
 
-//Récupération d'un seul utilisateur
+exports.getAllUsers = async (req, res) => {
+    try {
+        let query = 'SELECT name, image_profil, poste FROM users';
+        let [rows, fields] = await db.query(query);
+        let id = rows[0].id;
+        console.log(rows)
+        res.status(200).json("utilisateurs affichés");
+    } catch (e) {
+        res.status(500).json(e);
+    }
+};
+
+//Récupération d'un seul utilisateur pour la mise en place du profil
+
+exports.getOneUser = async (req, res, next) => {
+    try {
+        let oneUser = `SELECT id, name, poste, image_profil, description FROM users WHERE id= '${req.params.id}'; `;
+        let [rows, fields] = await db.query(oneUser);
+        console.log('rows:', rows.length)
+        if (rows.length == 0) {
+            return res.status(401).json("utilisateurs introuvable");
+        } else {
+            return res.status(200).json("profil du user affiché");
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 //mise à jour du mot de passe de l'utilisateur
 
+exports.updatePasswordOfUser = async (req, res, next) => {
+    try {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+        let updatePassword = `UPDATE users SET password = '${hash}' WHERE id = '${req.params.id}';`;
+        let [rows, fields] = await db.query(updatePassword);
+        return res.status(201).json("mot de passe modifié");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
 //mise à jour de la photo de l'utilisateur
 
+exports.updatePhotoProfil = async (req, res, next) => {
+    try {
+        let photoProfil = `UPDATE users SET image_profil = '${req.protocol}://${req.get('host')}/images/${req.file.filename}' WHERE id = '${req.params.id}';`;
+        let [rows, fields] = await db.query(updatePhotoProfil);
+        return res.status(201).json("photo de profil modifiée");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+//mise à jour  de la description
+
+exports.UpdateDescription = async (req, res, next) => {
+    try {
+        let descriptions = `UPDATE users SET description = '${req.body.description}' WHERE id = '${req.params.id}';`;
+        let [rows, fields] = await db.query(descriptions);
+        return res.status(200).json("modification de la description");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+//mise à jour du poste de l'utilisateur
+
+exports.updatePoste = async (req, res, next) => {
+    try {
+        let poste = `UPDATE users SET poste = '${req.body.poste}' WHERE id = '${req.params.id}';`;
+        let [rows, fields] = await db.query(poste);
+        return res.status(200).json("l'utilisateur a changer de poste");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
 //supression du compte de l'utilisateur
+
+exports.deleteUser = async (req, res, next) => {
+    try {
+        let deleteCountOfUser = `DELETE FROM users WHERE id = '${req.params.id}';`;
+        let [rows, fields] = await db.query(deleteCountOfUser);
+
+        //si le tableau rows contient quelque chose on supprime le compte
+        if (rows.length > 0) {
+            return res.status(200).json("le compte a été supprimé");
+        } else {
+            return res.status(404).json("l'utilisateur n'existe pas ou plus");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 //comment gérer le compte admin ?
--- back-end/route/usersRoute.js
+++ back-end/route/usersRoute.js
@@ -1,14 +1,47 @@
 express = require("express");
 const router = express.Router();
 
-const {getusers} = require("../controller/usersController");
+const {UpdateDescription} = require("../controller/usersController");
+
+const {updatePoste} = require("../controller/usersController");
+
+const {getAllUsers} = require("../controller/usersController");
 
 const {signup} = require("../controller/usersController");
 
 const {login} = require("../controller/usersController");
 
-router.get("/", getusers);
+const {getOneUser} = require("../controller/usersController");
+
+const {updatePasswordOfUser} = require("../controller/usersController");
+
+const {deleteUser} = require("../controller/usersController");
+
+const {updatePhotoProfil} = require("../controller/usersController");
+
+//la page qui va afficher l'organigramme de l'entreprise
+router.get("/organigrame", getAllUsers);
+
+//login et inscription
 router.post("/signup", signup);
-router.post("/login", login)
+router.post("/login", login);
+
+//affichage de la page profil
+router.get("/:id", getOneUser);
+
+//changement du mot de passe du user
+router.put("/:id", updatePasswordOfUser);
+
+//changement de la photo de profil du user
+router.put("/:id", updatePhotoProfil);
+
+//mise à jour de la description
+router.put("/:id/description", UpdateDescription);
+
+//m'utilisateur a changé de poste
+router.put("/:id/poste", updatePoste);
+
+//suppression du compte utilisateur
+router.delete("/:id", deleteUser);
 
 module.exports = router;
