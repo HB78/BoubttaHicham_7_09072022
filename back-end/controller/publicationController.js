a15a8389b54ff263b1b0c39e59e4f68272f60688
HB78 <78313137+HB78@users.noreply.github.com>
2022-07-27 16:54:38 +0200

    mise en place de la route getAllPublication dans publicationController

--- back-end/controller/publicationController.js
+++ back-end/controller/publicationController.js
@@ -1,5 +1,5 @@
 //c'est la page principale qui va afficher toutes les publications
-
+const db = require("../dataBase/db");
 //afficher toutes les publications
 
 //TODO: afficher les publications avec leur like, message et contenu
@@ -7,10 +7,36 @@
 exports.getAllPublication = async (req, res, next) => {
     try {
         //les publications sans commentaires vont elles s'afficher ?
-        let publications = `SELECT users.id, users.nom, users.image_profil AS users, publication.id, publication.title, publication.contenu, publication.image_path, publication.date_publi AS publication,   
-        id.commentaire, contenu.commentaire, date_comment.commentaire AS commentaire, id.reaction, love.reaction AS likes;`;
-        let [rows, fields] = await db.query(publications);
-        console.log("--> dans le rows il y a tt les publications", rows)
+        let query = `SELECT DISTINCT publication.id, publication.title, publication.date_publi, users.name AS userName, 
+            (
+                select count(*) 
+                from reaction 
+                WHERE reaction.love = 1 
+                and reaction.id_publi = publication.id
+            ) as alike,
+            (
+                select count(*) 
+                from reaction 
+                WHERE reaction.love = -1
+                and reaction.id_publi = publication.id
+            ) as dislike
+        from publication
+        left join reaction on reaction.id_publi = publication.id
+        left join users on reaction.id_publi = users.id;`;
+        let [publications, fields] = await db.query(query);
+        // console.log("--> dans le rows il y a tt les publications", rows)
+
+        console.log(publications[4])
+
+        for (let i = 0 ; i < publications.length ; i++) {
+            const publi = publications[i]
+            const publi_id = publi.id
+            const requete = `select commentaire.id, contenu, date_comment, users.name from commentaire join users on users.id = commentaire.id_user where id_publi = ` + publi_id 
+            let [res_commentaires, fields] = await db.query(requete);
+   
+            publications[i].commentaires = res_commentaires
+        }
+        console.log(publications[4])
         return res.status(200).json("toutes les publications sont affichées");
     } catch (error) {
         res.status(500).json(error);
@@ -22,10 +48,10 @@ exports.createPublication = async (req, res, next) => {
     try {
         let createPostWithImage = `INSERT INTO publication (title, contenu, image_path, date_publi) VALUES 
         '${req.body.title}', '${req.body.contenu}', '${req.protocol}://${req.get('host')}/images/${req.file.filename}', 
-        '${req.body.date_publi}'WHERE id = '${req.params.id}';`;
+        '${req.body.date_publi};`;
 
         let createPostWithNoImage = `INSERT INTO publication (title, contenu, date_publi) VALUES 
-        '${req.body.title}', '${req.body.contenu}','${req.body.date_publi}' id = '${req.params.id}';`;
+        '${req.body.title}', '${req.body.contenu}','${req.body.date_publi}';`;
 
         //si la requète contient un fichier
         if(req.file) {
@@ -46,7 +72,7 @@ exports.createPublication = async (req, res, next) => {
 //effacer une publication
 exports.deletePublication = async (req, res) => {
     try {
-        let deletePost = `DELETE FROM publication WHERE publication.id = '${req.params.publication.id}';`;
+        let deletePost = `DELETE FROM publication WHERE publication.id = '${req.params.id}';`;
         let [rows, fields] = await db.query(deletePost);
         console.log("--> test delete publication", rows)
         return res.status(200).json("la publication a été effacée");
@@ -64,6 +90,9 @@ exports.updatePublication = async (req, res) => {
             let updatePost = `UPDATE publication SET image_path = '${req.protocol}://${req.get('host')}/images/${req.file.filename}',
             title ='${req.params.title}', contenu = '${req.params.contenu}', date_publi = '${req.params.date_publi}'  WHERE id = '${req.params.id}';`;
             let [rows, fields] = await db.query(updatePost);
+
+            
+
             console.log("--> update publi with photo", rows)
         }else {
             let updatePostNoImage = `UPDATE publication SET title ='${req.params.title}', contenu = '${req.params.contenu}', 
